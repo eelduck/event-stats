7cfda952872cd31c479c9235e02946f273eb57cb
R K <wohuavt@gmail.com>
2021-06-25 12:29:51 +0200

    rearrange imports

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
-from core.models import User
 from django.conf import settings
-from django.contrib import admin, messages
+from django.contrib import messages, admin
 from django.contrib.admin.options import IS_POPUP_VAR
 from django.contrib.admin.utils import unquote
 from django.contrib.auth import update_session_auth_hash
@@ -18,8 +17,11 @@ from django.utils.translation import gettext, gettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
 
+from core.models import User
+
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -66,12 +68,12 @@ class UserAdmin(admin.ModelAdmin):
 
     def get_urls(self):
         return [
-            path(
-                '<id>/password/',
-                self.admin_site.admin_view(self.user_change_password),
-                name='auth_user_password_change',
-            ),
-        ] + super().get_urls()
+                   path(
+                       '<id>/password/',
+                       self.admin_site.admin_view(self.user_change_password),
+                       name='auth_user_password_change',
+                   ),
+               ] + super().get_urls()
 
     def lookup_allowed(self, lookup, value):
         # Don't allow lookups involving passwords.
