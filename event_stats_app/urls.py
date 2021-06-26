aa8da4c254123b420aa0d28f2907a8b2cae2114e
dimon.bat <dimon.bat@yandex.ru>
2021-06-26 19:23:18 +0500

    Эксперименты по вьюхам статистики

--- event_stats_app/urls.py
+++ event_stats_app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-
+    path('stat/user/', views.userstat, name='userstat'),
+    path('stat/event/', views.eventstat, name='eventstat'),
+    path('stat/user/<int:user_id>', views.userdetail, name='userdetail'),
 ]
--- event_stats_app/views.py
+++ event_stats_app/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from .models import User
+from .models import Event
 
-# Create your views here.
+# users statistic
+def userstat(request):
+    users = User.objects.order_by('email')
+    template = loader.get_template('stats/userstat.html')
+    context = {
+        'users_list': users,
+    }
+    return HttpResponse(template.render(context, request))
+
+# detail user statistic
+def userdetail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'stat/userdetail.html', {'user': user})
+
+
+# events statistic
+def eventstat(request):
+    events = Event.objects.order_by('id')
+    template = loader.get_template('stats/eventstat.html')
+    context = {
+        'events_list': events,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+# detail event
+def eventdetail(request, event_id):
+    event = get_object_or_404(User, pk=event_id)
+    return render(request, 'stat/eventdetail.html', {'event': event})
+
+
+# tracks statistic
+def eventstat(request):
+    events = Event.objects.order_by('id')
+    template = loader.get_template('stats/eventstat.html')
+    context = {
+        'events_list': events,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+# detail event
+def eventdetail(request, event_id):
+    event = get_object_or_404(User, pk=event_id)
+    return render(request, 'stat/eventdetail.html', {'event': event})
