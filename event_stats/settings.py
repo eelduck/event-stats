fbf38b4a8e29ce50970ab4c155f366162906c5b2
Azamatov Ildar <xset55@gmail.com>
2021-06-30 02:18:43 +0400

    add simpleui

--- event_stats/settings.py
+++ event_stats/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'event_stats.apps.CustomAdminConfig',
     # 'django.contrib.admin',
     'django.contrib.auth',
